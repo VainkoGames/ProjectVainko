5d64599cd7653bdb3863e79b7b942fffea2739fa
Pawan <pavansai432@gmail.com>
2022-01-14 13:58:08 +0530

    Project Folder Management , New & Final Character Added With One Unlit Material

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float mSpeed;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
+            this.gameObject.GetComponent<Animator>().SetBool("RUN", true);
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                this.gameObject.GetComponent<Animator>().SetTrigger("FRONTFLIP");
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("RUN", false);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        Application.targetFrameRate = 30;
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this); //Singleton
+            return;
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void Start()
+    {
+                
+    }
+}
